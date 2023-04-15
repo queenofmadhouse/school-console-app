f7a1623e8f253aab7e921fbbf0646bf6043d4062
Evgenii Chendev <banerlave@gmail.com>
2023-04-15 23:02:50 +0300

    ApplicationRunner update;legacy files deleted

--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/ApplicationRunner.java
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/ApplicationRunner.java
@@ -1,44 +1,23 @@
 package com.foxminded.chendev.schoolconsoleapp;
 
 import com.foxminded.chendev.schoolconsoleapp.controller.MenuController;
-import com.foxminded.chendev.schoolconsoleapp.controller.validator.InputValidator;
-import com.foxminded.chendev.schoolconsoleapp.controller.validator.Validator;
-import com.foxminded.chendev.schoolconsoleapp.service.GroupService;
-import com.foxminded.chendev.schoolconsoleapp.service.StudentService;
-import com.foxminded.chendev.schoolconsoleapp.service.impl.CourseServiceImpl;
-import com.foxminded.chendev.schoolconsoleapp.service.impl.GroupServiceImpl;
-import com.foxminded.chendev.schoolconsoleapp.service.impl.StudentServiceImpl;
-import com.foxminded.chendev.schoolconsoleapp.view.ConsoleHandler;
-import com.foxminded.chendev.schoolconsoleapp.view.ConsoleHandlerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-
         SpringApplication app = new SpringApplication(ApplicationRunner.class);
         app.setWebApplicationType(WebApplicationType.NONE);
 
         ApplicationContext applicationContext = SpringApplication.run(ApplicationRunner.class, args);
 
-        StudentService studentService = applicationContext.getBean(StudentServiceImpl.class);
-        GroupService groupService = applicationContext.getBean(GroupServiceImpl.class);
-        CourseServiceImpl courseService = applicationContext.getBean(CourseServiceImpl.class);
-
-        Validator validator = new InputValidator();
-        ConsoleHandler consoleHandler = new ConsoleHandlerImpl(new Scanner(System.in));
-
-        MenuController menuController = new MenuController(studentService, groupService, courseService,
-                validator, consoleHandler);
+        MenuController menuController = applicationContext.getBean(MenuController.class);
 
         menuController.provideMenu();
-
     }
 }
--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/reader/FileReader.java
+++
@@ -1,6 +0,0 @@
-package com.foxminded.chendev.schoolconsoleapp.reader;
-
-public interface FileReader {
-
-    String readFile(String filePath);
-}
