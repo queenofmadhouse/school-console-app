db44b1d6ab80ad17ab41a504048325bce44bfa8b
Evgenii Chendev <evgenii.chendev@proton.me>
2023-04-17 03:05:08 +0300

    now we use Flyway Migration; sql for app tables create deleted (from java/src/...)

--- school-console-app/src/main/resources/sql/students_courses_relation.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS school.students_courses_relation;
-
-CREATE TABLE IF NOT EXISTS school.students_courses_relation
-(
-    student_id integer NOT NULL,
-    course_id  integer NOT NULL
-)
