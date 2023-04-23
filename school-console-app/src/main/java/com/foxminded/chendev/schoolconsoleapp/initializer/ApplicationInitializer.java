ec610b64f6aed8672df5802c9c7acb98e27e9e97
Evgenii Chendev <evgenii.chendev@proton.me>
2023-04-23 03:23:38 +0300

    Row ApplicationInitializer added

---
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/initializer/ApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.foxminded.chendev.schoolconsoleapp.initializer;
+
+import javax.annotation.PostConstruct;
+
+public interface ApplicationInitializer {
+
+//    @PostConstruct
+    void init();
+}
