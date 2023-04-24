7a86d2e35c3a317564500dd201ddbaea689c1616
Evgenii Chendev <evgenii.chendev@proton.me>
2023-04-24 22:39:43 +0300

    ApplicationInitializerImpl corrected

--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/initializer/ApplicationInitializerImpl.java
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/initializer/ApplicationInitializerImpl.java
@@ -1,33 +1,38 @@
 package com.foxminded.chendev.schoolconsoleapp.initializer;
 
-import com.foxminded.chendev.schoolconsoleapp.dao.CourseDao;
-import com.foxminded.chendev.schoolconsoleapp.dao.GroupDao;
-import com.foxminded.chendev.schoolconsoleapp.dao.StudentDao;
-import com.foxminded.chendev.schoolconsoleapp.dao.impl.StudentDaoImpl;
+import com.foxminded.chendev.schoolconsoleapp.dao.CrudDao;
 import com.foxminded.chendev.schoolconsoleapp.service.DataGeneratorService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Service
 public class ApplicationInitializerImpl implements ApplicationInitializer {
 
-    private final StudentDao studentDao;
-    private final GroupDao groupDao;
-    private final CourseDao courseDao;
+    private final List<CrudDao<?>> daoList;
     private final DataGeneratorService dataGeneratorService;
 
-    public ApplicationInitializerImpl(StudentDao studentDao, GroupDao groupDao,
-                                      CourseDao courseDao, DataGeneratorService dataGeneratorService) {
-        this.studentDao = studentDao;
-        this.groupDao = groupDao;
-        this.courseDao = courseDao;
+    public ApplicationInitializerImpl(List<CrudDao<?>> daoList, DataGeneratorService dataGeneratorService) {
+        this.daoList = daoList;
         this.dataGeneratorService = dataGeneratorService;
     }
 
     @Override
     @PostConstruct
     public void init() {
-        dataGeneratorService.generateData();
+
+        if (!areAllDatabasesEmpty()) {
+            dataGeneratorService.generateData();
+        }
+    }
+
+    private boolean areAllDatabasesEmpty() {
+        return daoList.stream().allMatch(this::isTableEmpty);
+    }
+
+    private boolean isTableEmpty(CrudDao<?> dao) {
+        List<?> list = dao.findAll();
+        return list.isEmpty();
     }
 }
