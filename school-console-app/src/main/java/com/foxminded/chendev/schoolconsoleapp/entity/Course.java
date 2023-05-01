a8bb512b9b6140377a944464d957b695d026d4c7
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-01 15:45:15 +0300

    entity hotfix
    entity update (now use @Entity)

--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/entity/Course.java
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/entity/Course.java
@@ -1,15 +1,35 @@
 package com.foxminded.chendev.schoolconsoleapp.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "courses", schema = "school")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_id")
     private long courseId;
+
+    @Column(name = "course_name")
     private String courseName;
+
+    @Column(name = "course_description")
     private String courseDescription;
 
     private Course(Builder builder) {
         this.courseId = builder.courseId;
         this.courseName = builder.courseName;
         this.courseDescription = builder.courseDescription;
+    }
+
+    protected Course() {
+
     }
 
     public static Builder builder() {
--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/entity/Group.java
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/entity/Group.java
@@ -1,14 +1,33 @@
 package com.foxminded.chendev.schoolconsoleapp.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "groups", schema = "school")
 public class Group {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_id")
     private long groupId;
+
+    @Column(name = "group_name")
     private String groupName;
 
     private Group(Builder builder) {
         this.groupId = builder.groupId;
         this.groupName = builder.groupName;
     }
+
+    protected Group() {
+
+    }
+
 
     public static Builder builder() {
         return new Builder();
