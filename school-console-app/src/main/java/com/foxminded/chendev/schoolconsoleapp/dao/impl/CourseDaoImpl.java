1e97f2e92230e16785269b8e2aa5d67e63873312
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-02 02:51:45 +0300

    refactoring

--- school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/dao/impl/CourseDaoImpl.java
+++ school-console-app/src/main/java/com/foxminded/chendev/schoolconsoleapp/dao/impl/CourseDaoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,14 +18,10 @@ public class CourseDaoImpl extends AbstractCrudDao<Course> implements CourseDao 
 
     private static final String SELECT_ALL_COURSES = "SELECT g FROM Course g";
     private static final String SELECT_COURSE_BY_NAME = "SELECT c FROM Course c WHERE c.courseName = :course_name";
-
-//    private static final String SELECT_ALL_COURSES_BY_STUDENT_ID = "SELECT school.courses.*, school.students_courses_relation.user_id " +
-//            "FROM school.courses " +
-//            "INNER JOIN school.students_courses_relation ON school.courses.course_id = school.students_courses_relation.course_id " +
-//            "WHERE school.students_courses_relation.user_id = ?";
-    private static final String SELECT_ALL_COURSES_BY_STUDENT_ID = "SELECT c FROM Course c JOIN c.students s WHERE s.userId = : user_id";
+    private static final String SELECT_ALL_COURSES_BY_STUDENT_ID = "SELECT c FROM Course c JOIN c.students s " +
+            "WHERE s.userId = : user_id";
     private static final String DELETE_ALL_RELATIONS_BY_COURSE_ID = "DELETE FROM school.students_courses_relation" +
-            " WHERE course_id = ?";
+            " WHERE course_id = :course_id";
     private static final Logger logger = LoggerFactory.getLogger(CourseDaoImpl.class);
 
     public CourseDaoImpl(EntityManager entityManager) {
@@ -40,12 +37,17 @@ public class CourseDaoImpl extends AbstractCrudDao<Course> implements CourseDao 
     @Transactional
     public List<Course> findCoursesByStudentId(long studentId) {
         try {
+
             List<Course> resultList = entityManager.createQuery(SELECT_ALL_COURSES_BY_STUDENT_ID, Course.class)
                     .setParameter("user_id", studentId).getResultList();
+
             logger.info("Method findCoursesByStudentId was cold with parameters: " + studentId);
+
             return resultList;
         } catch (RuntimeException e) {
+
             logger.error("Exception in method findCoursesByStudentId with parameters: " + studentId, e);
+
             throw new DataBaseRuntimeException("Can't find course by student id: " + studentId, e);
         }
     }
@@ -54,10 +56,15 @@ public class CourseDaoImpl extends AbstractCrudDao<Course> implements CourseDao 
     @Transactional
     public void deleteAllRelationsByCourseId(long courseId) {
         try {
-            entityManager.createNativeQuery(DELETE_ALL_RELATIONS_BY_COURSE_ID).setParameter(1, courseId).executeUpdate();
+
+            entityManager.createNativeQuery(DELETE_ALL_RELATIONS_BY_COURSE_ID)
+                    .setParameter("course_id", courseId).executeUpdate();
+
             logger.info("Method deleteAllRelationsByCourseId was cold with parameters: " + courseId);
         } catch (RuntimeException e) {
+
             logger.error("Exception in method deleteAllRelationsByCourseId with parameters: " + courseId, e);
+
             throw new DataBaseRuntimeException("Can't delete all relations by course id: " + courseId, e);
         }
     }
@@ -67,12 +74,19 @@ public class CourseDaoImpl extends AbstractCrudDao<Course> implements CourseDao 
     public Optional<Course> findCourseByName(String courseName) {
         try {
 
-            Optional<Course> resultOptional = Optional.ofNullable(entityManager.createQuery(SELECT_COURSE_BY_NAME, Course.class)
-                        .setParameter("course_name", courseName).getSingleResult());
+            Optional<Course> resultOptional = Optional.ofNullable(
+                    entityManager.createQuery(SELECT_COURSE_BY_NAME, Course.class)
+                    .setParameter("course_name", courseName).getSingleResult()
+            );
 
             logger.info("Method findCourseByName was cold with parameters: " + courseName);
 
             return resultOptional;
+        } catch (NoResultException e) {
+
+            logger.warn("Can't find by name: " + courseName);
+
+            return Optional.empty();
         } catch (RuntimeException e) {
 
             logger.error("Exception in method findCourseByName with parameters: " + courseName);
